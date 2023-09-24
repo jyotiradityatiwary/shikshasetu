cd7e27e5f13764a399e28fcabef1da32aa6f8cb7
NamishAgrawal <34449852+NamishAgrawal@users.noreply.github.com>
2023-09-24 06:51:55 +0530

    Update parsing-function.js

--- database/parsing-function.js
+++ database/parsing-function.js
@@ -1,26 +1,25 @@
-let stri;
-let objj = [ { topic: 'topic1', city: 'city1'}, { topic: 'topic2', city: 'city2'}, { topic: 'topic3', city: 'city3'}, { topic: 'topic4', city: 'city4'}, { topic: 'topic5', city: 'city5'}, { topic: 'topic6', city: 'city6'} ]
-let str = 'topic1;city1;topic2;city2;topic3;city3;topic4;city4;topic5;city5;topic6;city6;'
+let objj = [ { topic: 'topic1', city: 'city1',description: 'description 1'}, { topic: 'topic2', city: 'city2',description:'description2'}, { topic: 'topic3', city: 'city3',description:'description3'}, { topic: 'topic4', city: 'city4', description:'description4'}, { topic: 'topic5', city: 'city5',description: 'description5'}, { topic: 'topic6', city: 'city6',description:'description6'} ]
+let str = 'topic1;city1;description 1;topic2;city2;description2;topic3;city3;description3;topic4;city4;description4;topic5;city5;description5;topic6;city6;description6;'
 
 function to_obj(str){
     let arr = str.split(';');
     let obj = [];
-    for(let i = 0; i < arr.length-1; i+=2){
-        obj.push({topic: arr[i], city: arr[i+1]});
+    for(let i = 0; i < arr.length-1; i+=3){
+        obj.push({topic: arr[i], city: arr[i+1],description: arr[i+2]});
     }
     return obj;
 }
 
 let obj =to_obj(str);
 console.dir(obj);
-for(let i =0;i<5;i++){
-    console.log(obj[i]);
-}
+// for(let i =0;i<5;i++){
+//     console.log(obj[i]);
+// }
 
 function to_string2(obj){
     let stri = '';
     for(let i = 0; i < obj.length; i++){
-        stri += obj[i].topic + ';' + obj[i].city + ';';
+        stri += obj[i].topic + ';' + obj[i].city + ';'+obj[i].description + ';' ;
     }
     return stri;
 }
